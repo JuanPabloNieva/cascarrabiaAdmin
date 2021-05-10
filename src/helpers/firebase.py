ca0ddd916167b7f5a6f5fa2b2ad1f2100f98e154
JuanPabloNieva <juanpablonieva@outlook.com>
2021-05-09 23:15:32 -0300

    CRUD categorias-productos

--- src/helpers/firebase.py
+++ src/helpers/firebase.py
@@ -1,15 +1,17 @@
 import pyrebase
 import os
 
+from config import *
+
 config = {
-    "apiKey": "AIzaSyCKPFF6HfcmmKHeH_KBdTzjKZt67fImjkg",
-    "authDomain": "testapi-d2ef9.firebaseapp.com",
-    "databaseURL": "https://testapi-d2ef9-default-rtdb.firebaseio.com",
-    "projectId": "testapi-d2ef9",
-    "storageBucket": "testapi-d2ef9.appspot.com",
-    "messagingSenderId": "101372487685",
-    "appId": "1:101372487685:web:f62f0a6d1c7b80149c946c",
-    "measurementId": "G-XXC67W3F4B"
+    "apiKey": API_KEY,
+    "authDomain": AUTH_DOMAIN,
+    "databaseURL": DATABASE_URL,
+    "projectId": PROJECT_ID,
+    "storageBucket": STORAGE_BUCKET,
+    "messagingSenderId": MESSAGING_SENDER_ID,
+    "appId": APP_ID,
+    "measurementId": MEASUREMENT_ID
 }
 
 firebase = pyrebase.initialize_app(config)
